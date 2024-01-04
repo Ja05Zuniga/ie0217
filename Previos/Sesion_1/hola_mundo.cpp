52a79f1465c457bcd23c3c26dd94c5b50588f2ef
Ja05Zuniga <135482938+Ja05Zuniga@users.noreply.github.com>
2024-01-04 14:17:26 -0600

    Los previos de la primera sesión
    Signed-off-by: Ja05Zuniga <135482938+Ja05Zuniga@users.noreply.github.com>

---
+++ Previos/Sesion_1/hola_mundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int var1 = 2;
+    int var2;
+    var2 = (var1 >= 0) ? 15 : 27;
+    // variable = (condición) ? si sí : si no;
+
+    cout<<"Hola mundo";
+    cout << "\nvar2: " << var2;
+    
+    return 0;
+}
---
+++ Previos/Sesion_1/otro.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Otro mundo\n";
+    cout<<"Otro mundo perro";
+
+    return 0;
+}
