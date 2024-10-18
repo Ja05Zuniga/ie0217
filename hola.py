9b1ef7e19a38e4f96b2e855be2bbae13efadcff1
Ja05Zuniga <jean.zunigamendoza@ucr.ac.cr>
2024-10-18 14:13:54 -0600

    Hola papa

--- hola.py
+++ hola.py
@@ -1,6 +1,5 @@
-while =!"."
-    hola = int(input("Ingrese un numero: "))
-    if hola < 5 :
-        print("menor")
-    else:
-        print("Mayor")
+hola = int(input("Ingrese un numero: "))
+if hola < 5 :
+    print("menor")
+else:
+    print("Mayor")
